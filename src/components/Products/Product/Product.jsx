a7941bf47f62d9e3fdd18cef4149de47b5e2ca0d
mrhrifat <mrhrifat383759@gmail.com>
2021-12-29 17:29:58 +0600

    Create a functional based component & import some from material ui

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Card, CardContent, CardMedia, CardActions, Typography, IconButton } from '@mui/material'
+// import { AddShoppingCard } from '@mui/material/SvgIcon'
+import { AddShoppingCard } from '@mui/material/Icon'
 
 const Product = () => {
     return (
         <div>
-            
+
         </div>
     )
 }
