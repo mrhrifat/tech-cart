d71f91405f70db62058c9b514f6741734b12efdc
mrhrifat <mrhrifat383759@gmail.com>
2021-12-29 20:09:12 +0600

    Create full jsx & import Logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
-
+import { AppBar, Menu, MenuItem, Toolbar, Typography, IconButton, Badge } from '@mui/material'
+import { ShoppingCart } from '@mui/icons-material'
+import Logo from '../../images'
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <AppBar className={classes.appBar} position='fixed' color='inherit'>
+                <Toolbar>
+                    <Typography className={classes.title} variant='h6' color='inherit'>
+                        <img className={classes.image} alt='Tech Cart' src={Logo} height='25px' />
+                        Tech Cart
+                    </Typography>
+                    <div className={classes.grow} />
+                    <div className={classes.button}>
+                        <IconButton aria-label='Show cart products' color='inherit'>
+                            <Badge badgeContent={2} color='secondary'>
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </>
     )
 }
 
