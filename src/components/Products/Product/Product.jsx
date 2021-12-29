036bd9f0b7a437efd35cecbedb00c190fcffe56b
mrhrifat <mrhrifat383759@gmail.com>
2021-12-29 18:20:14 +0600

    Make complete the Product Card UI

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -3,11 +3,29 @@ import { Card, CardContent, CardMedia, CardActions, Typography, IconButton } fro
 // import { AddShoppingCard } from '@mui/material/SvgIcon'
 import { AddShoppingCard } from '@mui/material/Icon'
 
-const Product = () => {
+const Product = ({ product }) => {
     return (
-        <div>
-
-        </div>
+        <Card className={classes.root}>
+            <CardMedia className={classes.media} image='' title={product.name} />
+            <CardContent>
+                <div className={classes.cardContent}>
+                    <Typography variant='h5' gutterBottom>
+                        {product.name}
+                    </Typography>
+                    <Typography variant='h5'>
+                        {product.price}
+                    </Typography>
+                </div>
+                <Typography variant='h2' color='textSecondary'>
+                    {product.description}
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing className={classes.CardActions}>
+                <IconButton aria-label='Add to Cart'>
+                    <AddShoppingCard />
+                </IconButton>
+            </CardActions>
+        </Card>
     )
 }
 
