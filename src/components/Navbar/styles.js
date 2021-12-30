a8ed2dafcefd030733005edf4371f69f95823cd9
mrhrifat <mrhrifat383759@gmail.com>
2021-12-30 21:18:31 +0600

    Update styles code from gist with uncomments

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,16 +1,19 @@
-import {makeStyles} from '@mui/styles'
-import {alpha} from '@mui/material/styles'
+import {
+    makeStyles
+} from '@mui/styles'
+import {
+    alpha
+} from '@mui/material/styles'
+const drawerWidth = 0;
 
 export default makeStyles((theme) => ({
     appBar: {
         boxShadow: 'none',
         borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
-        // [theme.breakpoints.up('sm')]: {
-        //     width: `calc(100% - ${drawerWidth}px)`,
-        //     marginLeft: drawerWidth,
-        // },
-        width:theme.breakpoints.up('sm'),
-        marginLeft:theme.breakpoints.up('sm'),
+        [theme.breakpoints.up('sm')]: {
+            width: `calc(100% - ${drawerWidth}px)`,
+            marginLeft: drawerWidth,
+        },
     },
     title: {
         flexGrow: 1,
@@ -21,49 +24,49 @@ export default makeStyles((theme) => ({
     image: {
         marginRight: '10px',
     },
-    // menuButton: {
-    //     marginRight: theme.spacing(2),
-    //     [theme.breakpoints.up('sm')]: {
-    //         display: 'none',
-    //     },
-    // },
+    menuButton: {
+        marginRight: theme.spacing(2),
+        [theme.breakpoints.up('sm')]: {
+            display: 'none',
+        },
+    },
     grow: {
         flexGrow: 1,
     },
-    // search: {
-    //     position: 'relative',
-    //     borderRadius: theme.shape.borderRadius,
-    //     backgroundColor: alpha(theme.palette.common.white, 0.15),
-    //     '&:hover': {
-    //         backgroundColor: alpha(theme.palette.common.white, 0.25),
-    //     },
-    //     marginRight: theme.spacing(2),
-    //     marginLeft: 0,
-    //     width: '100%',
-    //     [theme.breakpoints.up('sm')]: {
-    //         width: 'auto',
-    //     },
-    // },
-    // searchIcon: {
-    //     padding: theme.spacing(0, 2),
-    //     height: '100%',
-    //     position: 'absolute',
-    //     pointerEvents: 'none',
-    //     display: 'flex',
-    //     alignItems: 'center',
-    //     justifyContent: 'center',
-    // },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
     inputRoot: {
         color: 'inherit',
     },
-    // inputInput: {
-    //     padding: theme.spacing(1, 1, 1, 0),
-    //     // vertical padding + font size from searchIcon
-    //     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    //     transition: theme.transitions.create('width'),
-    //     width: '100%',
-    //     [theme.breakpoints.up('md')]: {
-    //         width: '20ch',
-    //     },
-    // },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
+    },
 }));
