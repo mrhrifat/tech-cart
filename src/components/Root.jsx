217f013782272566992797fd523ab67b28bdee32
mrhrifat <mrhrifat383759@gmail.com>
2021-12-30 20:55:12 +0600

    Add Navbar component

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import Products from './Products/Products'
+import { Navbar, Products } from './'
 
 const Root = () => {
     return (
         <>
+            <Navbar />
             <Products />
         </>
     )
